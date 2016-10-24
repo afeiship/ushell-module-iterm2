68b7237d339b13ab88a290d14dcdab5fc6888978
feizheng <1290657123@qq.com>
2016-10-24 22:40:58 +0800

    intial

---
+++ index.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+## path:
+ROOT_PATH=$(dirname $BASH_SOURCE);
+
+test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash";
+
+unset ROOT_PATH;
